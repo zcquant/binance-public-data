275d1b01da062a104641be9e5c8651234e58645c
zcquant <umekoo98@gmail.com>
2024-11-21 22:15:28 +0800

    Update scripts

--- scripts/download.py
+++ scripts/download.py
@@ -11,7 +11,7 @@ _t = time.time()
 _need = 0
 for i in data.itertuples():
     print(i)
-    if i.symbol != "INJUSDT" and _need == 0:
+    if i.symbol != "FLUXUSDT" and _need == 0:
         continue
     # _need = 1
     os.system(f"python python/download-aggTrade.py -t um -s {i.symbol} -startDate 2024-11-09 -endDate 2024-11-09 -folder /mnt/Data/tmp/binance_public_data_zip/ -skip-monthly 1")
--- scripts/unzip.py
+++ scripts/unzip.py
@@ -13,14 +13,12 @@ def unzip_to_directory(zip_path, extract_to):
 
 # 示例使用
 
-path_root = "/mnt/Data/tmp/binance_public_data_zip/data/futures/um/daily/aggTrades"
+path_root = "/opt/binance_public_data_zip/data/futures/um/daily/aggTrades"
 
 for symbol in sorted(os.listdir(path_root)):
-    # print(f)
-    zip_path = f'{path_root}/{symbol}/{symbol}-aggTrades-2024-11-09.zip'  # ZIP文件路径
-    extract_to = f'/mnt/Data/tmp/binance_public_data/data/futures/um/daily/aggTrades/{symbol}'  # 解压目标文件夹
-    if not os.path.exists(zip_path):
-        print(f"not exist: {zip_path}")
-        continue
+    extract_to = f'/opt/binance_public_data/data/futures/um/daily/aggTrades/{symbol}'  # 解压目标文件夹
     os.makedirs(extract_to, exist_ok=True)
-    unzip_to_directory(zip_path, extract_to)
+    for f_date in sorted(os.listdir(f"{path_root}/{symbol}")):
+        zip_path = f'{path_root}/{symbol}/{f_date}'  # ZIP文件路径
+        print(f"Unzipping {zip_path} to {extract_to}")
+        unzip_to_directory(zip_path, extract_to)
